58cd6b05f1fc2d4b2426fd1fd08041f17efad926
abhishekkumar <me2abhisheklucky@gmail.com>
2026-07-14 14:33:11 +0530

    My Welcome Screen

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sub800',
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Demo"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          "Hello Aditya 👋",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
